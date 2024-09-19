a328182cbf3e0efb3168fc49d55a5d2050b2afb2
Arthur Duarte <tuiduarte@gmail.com>
2024-09-18 21:11:08 -0400

    feat(income): add routes for creating, updating, and deleting income

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path')
 
 // Connect to the SQLite database
-let dbPath = path.join(__dirname, "budget-manager.db");
+let dbPath = path.join(__dirname, '..', '..', "budget-manager.db");
 console.log(dbPath)
 let db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
