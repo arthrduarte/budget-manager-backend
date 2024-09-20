cb85c9b8919566f0351d095050364dccc03e5b1f
Arthur Duarte <tuiduarte@gmail.com>
2024-09-20 12:52:15 -0400

    fix(login): add bcrypt comparison to login

--- routes/login.js
+++ routes/login.js
@@ -8,23 +8,24 @@ require('dotenv').config();
 /* POST user login. */
 router.post('/', function (req, res, next) {
   const { email, password } = req.body
-  
+
   let sql = `SELECT * FROM user WHERE email = ?`
-  db.get(sql, [email], (err, row) => {
+  db.get(sql, [email], async (err, row) => {
     if (err)
       return res.status(500).json({ error: err.message })
-    
-    if (row) {
-      bcrypt.compare(password, row.password_hash, (err) => {
-        if (err) 
-          return res.status(500).json({ error: err.message });
 
-        const user = { id: row.id }
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-        res.json({accessToken: accessToken})
-      })
+    if (row) {
+      console.log(row)
+      const match = await bcrypt.compare(password, row.password_hash)
+      console.log(match)
+      if (!match)
+        return res.status(500).json({ error: 'Incorrect password' });
+
+      const user = { id: row.id }
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+      return res.status(200).json({ accessToken: accessToken })
     } else {
-      res.status(404).json({ error: 'User not found' });
+      res.status(404).json({ error: 'Email not registered' });
     }
   })
 });
